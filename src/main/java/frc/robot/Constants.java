f2693f2c4ec3d99fe73b871bed1a1d1aa94e2890
stewsters <stewsters@gmail.com>
2026-01-19 19:36:14 -0600

    Adding the drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,100 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class Swerve {
+    public static final int FRONT_LEFT = 0;
+    public static final int FRONT_RIGHT = 1;
+    public static final int BACK_LEFT = 2;
+    public static final int BACK_RIGHT = 3;
+    public static final double trackWidthX = Units.inchesToMeters(25.0);
+    public static final double trackWidthY = Units.inchesToMeters(25.0);
+    public static final double driveBaseRadius = Math.hypot(trackWidthX / 2.0, trackWidthY / 2.0);
+    public static final double maxLinearSpeed = Units.feetToMeters(14.5);
+    public static final double maxLinearAcceleration = Units.feetToMeters(10);
+    public static final double maxAngularSpeed = maxLinearSpeed / driveBaseRadius;
+    public static final double maxAngularAcceleration = maxLinearAcceleration / driveBaseRadius;
+    public static final Translation2d[] moduleTranslations = {
+            new Translation2d(trackWidthX / 2.0, trackWidthY / 2.0), // FL
+            new Translation2d(trackWidthX / 2.0, -trackWidthY / 2.0), // FR
+            new Translation2d(-trackWidthX / 2.0, trackWidthY / 2.0), // BL
+            new Translation2d(-trackWidthX / 2.0, -trackWidthY / 2.0) // BR
+    };
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(moduleTranslations);
+    public static final double linearDeadband = 0.1;
+    public static final double rotationalDeadband = 0.1;
+  }
+
+  public static final class Chute {
+    public static final int motorChannel = 11;
+    public static final int limitSwitchChannel = 3;
+    public static final double holdSpeed = 0.15;
+    public static final double shootSpeed = 0.3;
+  }
+
+  public static final class Intake {
+    public static final int frontMotorChannel = 15;
+    public static final double suckSpeed = .5;
+    public static final double shootSpeed = .2;
+  }
+
+  public static final class Pivot {
+    public static final int motorChannel = 17;
+    public static final int limitSwitchChannel = 9;
+    public static final double speed = 0.2;
+    public static final double outPosition = 30;
+    public static final double scorePosition = 10;
+  }
+
+  public static final class Elevator {
+    public static final int motorChannel = 13;
+    public static final int limitSwitchChannelTop = 0;
+    public static final int limitSwitchChannelBottom = 4;
+    public static final double downSpeed = .6;
+    public static final double upSpeed = .8;
+    public static final double l2Position = 32;
+    public static final double l3Position = 69;
+    public static final double l4Position = 118;
+    public static final double algae1Position = 15;
+    public static final double algae2Position = 54;
+    public static final double retractPosition = 200;
+  }
+
+  public static final class Extender {
+    public static final int motorChannel = 12;
+    public static final int limitSwitchChannel = 2;
+    public static final double speed = 0.5;
+    public static final double algaePosition = 20;
+    public static final double scorePosition = 0;
+  }
+
+  public static final class Climber {
+    public static final int motorChannel = 14;
+    public static final double speed = 1;
+    public static final double outMax = 261;
+  }
+
+  public static final class PoseEstimation {
+    public final static Vector<N3> stateStdDevs = VecBuilder.fill(0.1, 0.1, 0.1);
+    public final static Vector<N3> visionMeasurementStdDevs = VecBuilder.fill(.7, .7, 9999999);
+  }
+
+//  public static final class PathPlanner {
+//    public static final PPHolonomicDriveController controller = new PPHolonomicDriveController(
+//            new PIDConstants(.5, 0, 0), // 2.0 Translation constants 3
+//            new PIDConstants(3, 0, 0) // 1.3 Rotation constants 3
+//    );
+//    public static final RobotConfig config = new RobotConfig(15, 1, new ModuleConfig(0.0508, 3, 1.2, DCMotor.getNEO(1).withReduction((50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0)), 120.0, 1), trackWidthX);
+//  }
 }
---
+++ src/main/java/frc/robot/subsystems/PoseEstimationSubsystem.java
@@ -0,0 +1,141 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class PoseEstimationSubsystem extends SubsystemBase {
+    private final SwerveDrivePoseEstimator poseEstimator;
+    private final Supplier<Rotation2d> rotationSupplier;
+    private final Supplier<SwerveModulePosition[]> modulePositionSupplier;
+
+    private final Field2d field = new Field2d();
+    private String reefPosition = "right";
+    private int reefSide = 1;
+    private final List<Translation2d> waypoints = List.of(
+            new Translation2d(3, 4),
+            new Translation2d(3.75, 2.742),
+            new Translation2d(5.219, 2.742),
+            new Translation2d(5.956, 4),
+            new Translation2d(5.219, 5.316),
+            new Translation2d(3.75, 5.316),
+            new Translation2d(14.719, 4),
+            new Translation2d(13.969, 5.316),
+            new Translation2d(12.5, 5.316),
+            new Translation2d(11.75, 4),
+            new Translation2d(12.5, 2.742),
+            new Translation2d(13.969, 2.742)
+    );
+
+    public PoseEstimationSubsystem(Supplier<Rotation2d> rotationSupplier, Supplier<SwerveModulePosition[]> modulePositionSupplier) {
+        this.rotationSupplier = rotationSupplier;
+        this.modulePositionSupplier = modulePositionSupplier;
+
+        poseEstimator = new SwerveDrivePoseEstimator(
+                Constants.Swerve.kinematics,
+                rotationSupplier.get(),
+                modulePositionSupplier.get(),
+                new Pose2d(),
+                Constants.PoseEstimation.stateStdDevs,
+                Constants.PoseEstimation.visionMeasurementStdDevs);
+    }
+
+    @Override
+    public void periodic() {
+        poseEstimator.update(rotationSupplier.get(), modulePositionSupplier.get());
+
+        try {
+            LimelightHelpers.SetRobotOrientation("limelight", getCurrentPose().getRotation().getDegrees(), 0.0, 0.0, 0.0, 0.0, 0.0);
+
+            LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+  
+            if (limelightMeasurement.tagCount > 0) {
+                poseEstimator.addVisionMeasurement(
+                        limelightMeasurement.pose,
+                        limelightMeasurement.timestampSeconds);
+            } 
+        } catch (Exception e) {
+            // TODO: handle exception
+            DataLogManager.log(e.getMessage());
+        }
+
+        reefSide = waypoints.indexOf(getCurrentPose().getTranslation().nearest(waypoints)) % 6 + 1;
+        SmartDashboard.putNumber("Reef Side", reefSide);
+        SmartDashboard.putString("Coral Station", getCoralStation());
+
+        field.setRobotPose(getCurrentPose());
+        SmartDashboard.putData("Field", field);
+        SmartDashboard.putNumber("X pos", getCurrentPose().getX());
+        SmartDashboard.putNumber("Y pos", getCurrentPose().getY());
+    }
+
+    public Pose2d getCurrentPose() {
+        return poseEstimator.getEstimatedPosition();
+    }
+
+    public int getReefSide(){
+        return reefSide;
+    }
+
+    public String getReefPosition(){
+        return reefPosition;
+    }
+
+    public String getCoralStation(){
+        if (isOnRed()) {
+            return (getCurrentPose().getY() < 4) ? "Left Coral" : "Right Coral";
+        } else {
+            return (getCurrentPose().getY() > 4) ? "Left Coral" : "Right Coral";
+        }
+    }
+
+    public void setCurrentPose(Pose2d newPose) {
+        poseEstimator.resetPosition(rotationSupplier.get(), modulePositionSupplier.get(), newPose);
+    }
+
+    public void resetFieldPosition() {
+        setCurrentPose(new Pose2d());
+    }
+
+    public void resetDriveRotation() {
+        if (isOnRed()) {
+            poseEstimator.resetPosition(rotationSupplier.get(), modulePositionSupplier.get(), new Pose2d(getCurrentPose().getTranslation(), new Rotation2d(Math.PI)));
+        } else {
+            poseEstimator.resetPosition(rotationSupplier.get(), modulePositionSupplier.get(), new Pose2d(getCurrentPose().getTranslation(), new Rotation2d()));
+        }
+    }
+
+    public boolean isOnRed() {
+        var alliance = DriverStation.getAlliance();
+        return alliance.filter(value -> value == DriverStation.Alliance.Red).isPresent();
+    }
+
+    public void setReefPositionLeft(){
+        setReefPosition("left");
+    }
+
+    public void setReefPositionAlgae(){
+        setReefPosition("algae");
+    }
+
+    public void setReefPositionRight(){
+        setReefPosition("right");
+    }
+
+    public void setReefPosition(String reefPosition) {
+        this.reefPosition = reefPosition;
+        SmartDashboard.putString("Reef Position", this.reefPosition);
+    }
+}
