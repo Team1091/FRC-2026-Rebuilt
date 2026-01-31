e61dbc3ed4b784dd2b68a412ca16653024fb8144
stewsters <stewsters@gmail.com>
2026-01-31 10:36:40 -0600

    Adding some subsystems

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+
+    // shooter wheel motor
+    // angle adjuster
+    // indexing
+
+}
