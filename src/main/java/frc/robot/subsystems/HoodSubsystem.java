f06cc84e7d41738984d385e2b6a1727a2c8bb4f6
stewsters <stewsters@gmail.com>
2026-02-24 08:08:23 -0600

    Working on hood angle

--- src/main/java/frc/robot/subsystems/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/HoodSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,13 +10,18 @@ public class HoodSubsystem extends SubsystemBase {
 
     private final DigitalInput limitSwitchLow;
     private final DigitalInput limitSwitchHigh;
-    private double approxHoodAngle;
 
-    private static double MIN_HOOD_ANGLE = 100.00;
-    private static double MAX_HOOD_ANGLE = 100.00;
+    private double estimatedHoodAngle; // where we think we are
+    private double desiredHoodAngle; // where we want to be
+
+    private long lastTimeMs = System.currentTimeMillis();
+
+    private static double MIN_HOOD_ANGLE = 0.0;
+    private static double MAX_HOOD_ANGLE = 100.0;
+    private static double SPEED_PER_SECOND = 50.0; // 50 here means it will go up or down in 2 sec
 
     public HoodSubsystem() {
-        approxHoodAngle = 0;
+        estimatedHoodAngle = 0;
         if (Constants.Hood.disabled) {
             hoodMotor = null;
             limitSwitchLow = null;
@@ -26,25 +30,24 @@ public class HoodSubsystem extends SubsystemBase {
         }
 
         hoodMotor = new VictorSP(Constants.Hood.hoodMotorChannel);
-        limitSwitchLow = new DigitalInput(Constants.Hood.hoodLimitLow);
-        limitSwitchHigh = new DigitalInput(Constants.Hood.hoodLimitHigh);
+        limitSwitchLow = new DigitalInput(Constants.Hood.hoodLimitSwitchLow);
+        limitSwitchHigh = new DigitalInput(Constants.Hood.hoodLimitSwitchHigh);
     }
 
     public void resetEncoder() {
         if (Constants.Hood.disabled) return;
-        approxHoodAngle = 0.0;
-//        hoodEncoder.setPosition(0);
+        estimatedHoodAngle = 0.0; // start down, maybe we
     }
 
     public boolean isCloseEnoughToTarget() {
         if (Constants.Hood.disabled) {
             return true;
         }
-        return Math.abs(approxHoodAngle - hoodEncoder.getPosition()) < Constants.Hood.angleCloseEnough;
+        return Math.abs(estimatedHoodAngle - desiredHoodAngle) < Constants.Hood.angleCloseEnough;
     }
 
     public void setTargetAngle(double hoodAngle) {
-        this.approxHoodAngle = hoodAngle;
+        this.desiredHoodAngle = hoodAngle;
     }
 
     @Override
@@ -52,9 +55,38 @@ public class HoodSubsystem extends SubsystemBase {
         if (Constants.Hood.disabled) {
             return;
         }
-        var hoodPow = hoodController.calculate(hoodEncoder.getPosition(), approxHoodAngle);
-        hoodPow = MathUtil.clamp(hoodPow, -Constants.Hood.hoodMotorPower, Constants.Hood.hoodMotorPower);
-        hoodMotor.set(hoodPow);
+
+        // We are doing timing inside here, each time it gets through we calculate how long it's been
+        long currentTimeMs = System.currentTimeMillis();
+        double deltaTimeSeconds = ((double) (lastTimeMs - currentTimeMs)) / 1000.0;
+        lastTimeMs = currentTimeMs;
+
+
+        if (limitSwitchLow.get() && desiredHoodAngle <= estimatedHoodAngle) {
+            // we are hitting the low limit, stop
+            hoodMotor.set(0);
+            estimatedHoodAngle = MIN_HOOD_ANGLE;
+            return;
+
+        } else if (limitSwitchHigh.get() && desiredHoodAngle >= estimatedHoodAngle) {
+            // we are hitting the high limit, stop
+            hoodMotor.set(0);
+            estimatedHoodAngle = MAX_HOOD_ANGLE;
+            return;
+        } else if (isCloseEnoughToTarget()) {
+            hoodMotor.set(0);
+        } else {
+            // we are moving to the target
+
+            if (desiredHoodAngle > estimatedHoodAngle) {
+                estimatedHoodAngle += deltaTimeSeconds * SPEED_PER_SECOND;
+                hoodMotor.set(1);
+            } else {
+                estimatedHoodAngle -= deltaTimeSeconds * SPEED_PER_SECOND;
+                hoodMotor.set(-1);
+            }
+        }
+
     }
 
 }
