ef512cd307f80d0c0194ffa6d22207f1723d42a1
Tobey <05jerry05@gmail.com>
2026-02-25 07:36:07 -0600

    Don't need Servo Hood

--- src/main/java/frc/robot/subsystems/NewHoodSubsystem.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class NewHoodSubsystem extends SubsystemBase {
-    private static Servo hoodServo;
-}
